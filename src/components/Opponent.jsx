706d5df4b352a46dcf22752f1574f7f4c6aa3636
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-05-17 17:27:25 +0200

    styling changes

--- src/components/Opponent.jsx
+++ src/components/Opponent.jsx
@@ -1,110 +1,113 @@
-import React, { useState, useEffect } from 'react';
-import { fetchRandomPokemon } from '../utilities/FetchData';
-import { typeColors } from '../utilities/TypeColors';
+import React, { useState, useEffect } from "react";
+import { fetchRandomPokemon } from "../utilities/FetchData";
+import { typeColors } from "../utilities/TypeColors";
 
 const Opponent = ({ setOpponentPokemon, showStats, battleStarted }) => {
-	const [pokemon, setPokemon] = useState({});
-	const [displayedStats, setDisplayedStats] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+  const [pokemon, setPokemon] = useState({});
+  const [currentStat, setCurrentStat] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-	useEffect(() => {
-		const fetchAPI = async () => {
-			const randomPokemon = await fetchRandomPokemon();
-			setPokemon(randomPokemon);
-			setOpponentPokemon(randomPokemon);
-			setDisplayedStats([]);
-			setIsLoading(false);
-		};
-		fetchAPI();
-	}, [setOpponentPokemon]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const randomPokemon = await fetchRandomPokemon();
+      setPokemon(randomPokemon);
+      setOpponentPokemon(randomPokemon);
+      setCurrentStat(null);
+      setIsLoading(false);
+    };
+    fetchAPI();
+  }, [setOpponentPokemon]);
 
-	useEffect(() => {
-		if (showStats && pokemon.stats) {
-			const statsOrder = [
-				'attack',
-				'defense',
-				'health_points',
-				'special_attack',
-				'special_defense',
-				'speed',
-			];
+  useEffect(() => {
+    if (showStats && pokemon.stats) {
+      const statsOrder = [
+        "attack",
+        "defense",
+        "health_points",
+        "special_attack",
+        "special_defense",
+        "speed",
+      ];
 
-			const timer = setTimeout(() => {
-				const nextStat = statsOrder.find(
-					(stat) => !displayedStats.map(([name]) => name).includes(stat)
-				);
-				if (nextStat) {
-					setDisplayedStats((prevStats) => [
-						...prevStats,
-						[nextStat, pokemon.stats[nextStat]],
-					]);
-				}
-			}, 1000);
+      let currentIndex = 0;
 
-			return () => clearTimeout(timer);
-		}
-	}, [showStats, pokemon.stats, displayedStats]);
+      const intervalId = setInterval(() => {
+        if (currentIndex < statsOrder.length) {
+          const nextStat = statsOrder[currentIndex];
+          setCurrentStat([nextStat, pokemon.stats[nextStat]]);
+          currentIndex++;
+        } else {
+          clearInterval(intervalId);
+          setTimeout(() => {
+            setCurrentStat(null);
+          }, 4); // Hide the last stat after 1 second
+        }
+      }, 1000);
 
-	useEffect(() => {
-		setDisplayedStats([]);
-	}, [showStats]);
+      return () => clearInterval(intervalId);
+    }
+  }, [showStats, pokemon.stats]);
 
-	if (isLoading) {
-		return (
-			<div className='wrapper my-36'>
-				<div className='pokeball'></div>
-			</div>
-		);
-	}
+  useEffect(() => {
+    setCurrentStat(null);
+  }, [showStats]);
 
-	return (
-		<div className='flex flex-col w-96'>
-			<div>
-				<div className='flex justify-center'>
-					{pokemon ? (
-						<div
-							className='card w-[200px] h-[200px] shadow-xl'
-							style={{
-								background:
-									pokemon.type && pokemon.type.length === 1
-										? typeColors[pokemon.type[0]]
-										: pokemon.type && pokemon.type.length === 2
-										? `linear-gradient(to right, ${
-												typeColors[pokemon.type[0]]
-										  }, ${typeColors[pokemon.type[1]]})`
-										: 'transparent',
-							}}
-						>
-							<h2 className='pt-2 text-xl text-center mb-4'>{pokemon.name}</h2>
-							<figure>
-								<img
-									className='w-[100px] aspect-square object-cover animate-[wiggle_1s_ease-in-out_infinite]'
-									src={pokemon.image_url}
-									alt={pokemon.name}
-								/>
-							</figure>
-						</div>
-					) : (
-						<p>Loading...</p>
-					)}
-				</div>
+  if (isLoading) {
+    return (
+      <div className="wrapper my-36">
+        <div className="pokeball"></div>
+      </div>
+    );
+  }
 
-				<div className='flex justify-center'>
-					<div
-						className={`mt-6 italic flex flex-col font-extrabold`}
-						style={{ display: showStats ? 'block' : 'none' }}
-					>
-						{displayedStats.map(([name, value]) => (
-							<div key={name}>
-								<span>{name} :</span>
-								<span> {value}</span>
-							</div>
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-col w-96">
+      <div>
+        <div className="flex justify-center">
+          {pokemon ? (
+            <div
+              className="card w-[200px] h-[200px] shadow-xl"
+              style={{
+                background:
+                  pokemon.type && pokemon.type.length === 1
+                    ? typeColors[pokemon.type[0]]
+                    : pokemon.type && pokemon.type.length === 2
+                    ? `linear-gradient(to right, ${
+                        typeColors[pokemon.type[0]]
+                      }, ${typeColors[pokemon.type[1]]})`
+                    : "transparent",
+              }}
+            >
+              <h2 className="pt-2 text-xl text-center mb-4">{pokemon.name}</h2>
+              <figure>
+                <img
+                  className="w-[100px] aspect-square object-cover animate-[wiggle_1s_ease-in-out_infinite]"
+                  src={pokemon.image_url}
+                  alt={pokemon.name}
+                />
+              </figure>
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+
+        <div className="flex justify-center">
+          <div
+            className={`mt-6 italic flex flex-col font-extrabold`}
+            style={{ display: showStats ? "block" : "none" }}
+          >
+            {currentStat && (
+              <div>
+                <span>{currentStat[0]} :</span>
+                <span> {currentStat[1]}</span>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Opponent;
--- src/components/UserPokemon.jsx
+++ src/components/UserPokemon.jsx
@@ -1,117 +1,120 @@
-import { useEffect, useState } from 'react';
-import { fetchData } from '../utilities/FetchData';
-import { typeColors } from '../utilities/TypeColors';
+import { useEffect, useState } from "react";
+import { fetchData } from "../utilities/FetchData";
+import { typeColors } from "../utilities/TypeColors";
 
 const UserPokemon = ({
-	selectOnePoke,
-	selectedPokemon,
-	selectFromThumbnailPoke,
-	showStats,
+  selectOnePoke,
+  selectedPokemon,
+  selectFromThumbnailPoke,
+  showStats,
 }) => {
-	const [pokemonData, setPokemonData] = useState([]);
-	const [selectedPokeForFight, setSelectedPokeForFight] = useState(null);
-	const [displayedStats, setDisplayedStats] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [selectedPokeForFight, setSelectedPokeForFight] = useState(null);
+  const [displayedStat, setDisplayedStat] = useState(null);
 
-	useEffect(() => {
-		const fetchAPI = async () => {
-			const data = await fetchData();
-			setPokemonData(data);
-			const result = data.pokemons.filter(
-				(pokemon) => pokemon.name === selectOnePoke
-			);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const data = await fetchData();
+      setPokemonData(data);
+      const result = data.pokemons.filter(
+        (pokemon) => pokemon.name === selectOnePoke
+      );
 
-			if (selectFromThumbnailPoke) {
-				setSelectedPokeForFight(selectFromThumbnailPoke);
-				selectedPokemon(selectFromThumbnailPoke);
-			} else if (result[0] === undefined) {
-				let selectPokeString = localStorage.getItem('teamPokemons');
-				let selectPokeParsed = JSON.parse(selectPokeString);
+      if (selectFromThumbnailPoke) {
+        setSelectedPokeForFight(selectFromThumbnailPoke);
+        selectedPokemon(selectFromThumbnailPoke);
+      } else if (result[0] === undefined) {
+        let selectPokeString = localStorage.getItem("teamPokemons");
+        let selectPokeParsed = JSON.parse(selectPokeString);
 
-				setSelectedPokeForFight(selectPokeParsed[0]);
-				selectedPokemon(selectPokeParsed[0]);
-			} else {
-				setSelectedPokeForFight(result[0]);
-				selectedPokemon(result[0]);
-			}
-			setDisplayedStats([]);
-		};
-		fetchAPI();
-	}, [selectOnePoke, selectFromThumbnailPoke, selectedPokemon]);
+        setSelectedPokeForFight(selectPokeParsed[0]);
+        selectedPokemon(selectPokeParsed[0]);
+      } else {
+        setSelectedPokeForFight(result[0]);
+        selectedPokemon(result[0]);
+      }
+      setDisplayedStat(null);
+    };
+    fetchAPI();
+  }, [selectOnePoke, selectFromThumbnailPoke, selectedPokemon]);
 
-	useEffect(() => {
-		if (showStats && selectedPokeForFight.stats) {
-			const statsOrder = [
-				'attack',
-				'defense',
-				'health_points',
-				'special_attack',
-				'special_defense',
-				'speed',
-			];
+  useEffect(() => {
+    if (showStats && selectedPokeForFight && selectedPokeForFight.stats) {
+      const statsOrder = [
+        "attack",
+        "defense",
+        "health_points",
+        "special_attack",
+        "special_defense",
+        "speed",
+      ];
 
-			const timer = setTimeout(() => {
-				const nextStat = statsOrder.find(
-					(stat) => !displayedStats.map(([name]) => name).includes(stat)
-				);
+      let currentIndex = 0;
 
-				if (nextStat) {
-					setDisplayedStats((prevStats) => [
-						...prevStats,
-						[nextStat, selectedPokeForFight.stats[nextStat]],
-					]);
-				}
-			}, 1000);
-			return () => clearTimeout(timer);
-		}
-	}, [showStats, selectedPokeForFight, displayedStats]);
+      const intervalId = setInterval(() => {
+        if (currentIndex < statsOrder.length) {
+          const nextStat = statsOrder[currentIndex];
+          setDisplayedStat([nextStat, selectedPokeForFight.stats[nextStat]]);
+          currentIndex++;
+        } else {
+          clearInterval(intervalId);
+          setTimeout(() => {
+            setDisplayedStat(null);
+          }, 4); // Hide the last stat after 1 second
+        }
+      }, 1000);
 
-	return (
-		<div className='flex flex-col w-96 mb-4'>
-			<div className='flex justify-center'>
-				{selectedPokeForFight && (
-					<div
-						className='card w-[200px] h-[200px] shadow-xl'
-						style={{
-							background:
-								selectedPokeForFight.type.length === 1
-									? typeColors[selectedPokeForFight.type[0]]
-									: selectedPokeForFight.type.length === 2
-									? `linear-gradient(to right, ${
-											typeColors[selectedPokeForFight.type[0]]
-									  }, ${typeColors[selectedPokeForFight.type[1]]})`
-									: 'transparent',
-						}}
-					>
-						<h2 className='pt-2 text-xl text-center mb-4'>
-							{selectedPokeForFight.name}
-						</h2>
-						<figure>
-							<img
-								className='w-[100px] aspect-square object-cover'
-								src={selectedPokeForFight.image_url}
-								alt={selectedPokeForFight.name}
-							/>
-						</figure>
-					</div>
-				)}
-			</div>
-			{selectedPokeForFight && (
-				<div className='flex justify-center'>
-					<div
-						className={`mt-6 italic flex flex-col font-extrabold`}
-						style={{ display: showStats ? 'block' : 'none' }}
-					>
-						{displayedStats.map(([name, value]) => (
-							<div key={name}>
-								<span>{name} :</span>
-								<span> {value}</span>
-							</div>
-						))}
-					</div>
-				</div>
-			)}
-		</div>
-	);
+      return () => clearInterval(intervalId);
+    }
+  }, [showStats, selectedPokeForFight]);
+
+  return (
+    <div className="flex flex-col w-96 mb-4">
+      <div className="flex justify-center">
+        {selectedPokeForFight && (
+          <div
+            className="card w-[200px] h-[200px] shadow-xl"
+            style={{
+              background:
+                selectedPokeForFight.type.length === 1
+                  ? typeColors[selectedPokeForFight.type[0]]
+                  : selectedPokeForFight.type.length === 2
+                  ? `linear-gradient(to right, ${
+                      typeColors[selectedPokeForFight.type[0]]
+                    }, ${typeColors[selectedPokeForFight.type[1]]})`
+                  : "transparent",
+            }}
+          >
+            <h2 className="pt-2 text-xl text-center mb-4">
+              {selectedPokeForFight.name}
+            </h2>
+            <figure>
+              <img
+                className="w-[100px] aspect-square object-cover"
+                src={selectedPokeForFight.image_url}
+                alt={selectedPokeForFight.name}
+              />
+            </figure>
+          </div>
+        )}
+      </div>
+      {selectedPokeForFight && (
+        <div className="flex justify-center">
+          <div
+            className={`mt-6 italic flex flex-col font-extrabold`}
+            style={{ display: showStats ? "block" : "none" }}
+          >
+            {displayedStat && (
+              <div>
+                <span>{displayedStat[0]} :</span>
+                <span> {displayedStat[1]}</span>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserPokemon;
